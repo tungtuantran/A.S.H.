96c9100d2f2ccf2b0b00791d1c4f12f728a72d48
Tung Tuan Tran <licki_tung@hotmail.de>
2021-02-23 02:49:18 +0100

    removed comments from lightEstimation script

--- ASH iOS/Assets/LightEstimation.cs
+++ ASH iOS/Assets/LightEstimation.cs
@@ -11,17 +11,6 @@ public class LightEstimation : MonoBehaviour
     [SerializeField]
     private Light _light;
 
-    /*
-    [SerializeField]
-    private Text text1;
-
-    [SerializeField]
-    private Text text2;
-
-    [SerializeField]
-    private Text text3;
-    */
-
     private void Awake()
     {
         GameObject arCamera = GameObject.FindGameObjectWithTag("MainCamera");
@@ -43,21 +32,18 @@ public class LightEstimation : MonoBehaviour
         if (args.lightEstimation.averageBrightness.HasValue)
         {
             _light.intensity = args.lightEstimation.averageBrightness.Value;
-            //text1.text = $"Brightness: {args.lightEstimation.averageBrightness.Value}";
             Debug.Log("Average Brightness: " + args.lightEstimation.averageBrightness.Value);
         }
 
         if (args.lightEstimation.averageColorTemperature.HasValue)
         {
             _light.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
-            //text2.text = $"Temperature: {args.lightEstimation.averageColorTemperature.Value}";
             Debug.Log("Average Color Temperature: " + args.lightEstimation.averageColorTemperature.Value);
         }
 
         if (args.lightEstimation.colorCorrection.HasValue)
         {
-            _light.color = args.lightEstimation.colorCorrection.Value;              //_light.color ??
-            //text3.text = $"ColorCorrection: {args.lightEstimation.colorCorrection.Value}";
+            _light.color = args.lightEstimation.colorCorrection.Value;
             Debug.Log("Color Correction: " + args.lightEstimation.colorCorrection.Value);
         }
     }
