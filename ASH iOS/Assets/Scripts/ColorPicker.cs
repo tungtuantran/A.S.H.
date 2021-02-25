c344e6214af30841e316916807e0f37e7e4b574e
Tung Tuan Tran <licki_tung@hotmail.de>
2021-02-25 02:15:36 +0100

    fixed color picker script

--- ASH iOS/Assets/Scripts/ColorPicker.cs
+++ ASH iOS/Assets/Scripts/ColorPicker.cs
@@ -12,7 +12,7 @@ public class ColorEvent : UnityEvent<Color>
 {
 }
 
-public class ColorPicker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ColorPicker : MonoBehaviour //, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField]
     public Camera arCamera;
@@ -31,6 +31,7 @@ public class ColorPicker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     private bool pointerDown = false;
 
+    /*
     public void OnPointerDown(PointerEventData eventData)
     {
         pointerDown = true;
@@ -40,49 +41,61 @@ public class ColorPicker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         pointerDown = false;
     }
+    */
+
     // Start is called before the first frame update
     void Start()
     {
         Rect = GetComponent<RectTransform>();
         ColorTexture = GetComponent<Image>().mainTexture as Texture2D;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (RectTransformUtility.RectangleContainsScreenPoint(Rect, Input.mousePosition))                       //nur wenn mouse innherhalb des rectamgöes ist
+        if (Input.GetMouseButtonDown(0))
         {
-            if (pointerDown)
+            pointerDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            pointerDown = false;
+        }
+
+        if (pointerDown)
+        {
+            Vector2 delta;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(Rect, Input.mousePosition, arCamera, out delta);
+
+            Debug.Log("mousePosition: " + Input.mousePosition + "; Delta: " + delta);
+
+            float width = Rect.rect.width;
+            float height = Rect.rect.height;
+            delta += new Vector2(width * .5f, height * .5f);
+
+            //Debug.Log("offset delta:" + delta);
+
+            float x = Mathf.Clamp(delta.x / width, 0f, 1f);
+            float y = Mathf.Clamp(delta.y / height, 0f, 1f);
+            Debug.Log("x=" + x + " y=" + y);
+
+            int texX = Mathf.RoundToInt(x * ColorTexture.width);
+            int texY = Mathf.RoundToInt(y * ColorTexture.height);
+
+            Color color = ColorTexture.GetPixel(texX, texY);
+            if(color.a == 0)                                        //if mouse/pointer is over an alpha=0 pixel or if it's is not inside the color picker image
             {
-                Vector2 delta;
-                RectTransformUtility.ScreenPointToLocalPointInRectangle(Rect, Input.mousePosition, arCamera, out delta);
+                return;
+            }
+            color.a = 1;                                            //so its not transparent if mouse/pointer is over an alpha=0 pixel
+            colorText.color = color;
 
-                Debug.Log("mousePosition: " + Input.mousePosition + "; Delta: " + delta);
+            OnColorPreview?.Invoke(color);
 
-                float width = Rect.rect.width;
-                float height = Rect.rect.height;
-                delta += new Vector2(width * .5f, height * .5f);
-
-                //Debug.Log("offset delta:" + delta);
-
-                float x = Mathf.Clamp(delta.x / width, 0f, 1f);
-                float y = Mathf.Clamp(delta.y / height, 0f, 1f);
-                Debug.Log("x=" + x + " y=" + y);
-
-                int texX = Mathf.RoundToInt(x * ColorTexture.width);
-                int texY = Mathf.RoundToInt(y * ColorTexture.height);
-
-                Color color = ColorTexture.GetPixel(texX, texY);
-                color.a = 1;                                            //so its not transparent if mouse is over an alpha=0 pixel
-                colorText.color = color;
-
-                OnColorPreview?.Invoke(color);
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    OnColorSelect?.Invoke(color);
-                }
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnColorSelect?.Invoke(color);
             }
         }
     }
