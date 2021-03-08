51ac4466efd4091e08b62643a94c5bec85179862
Tung Tuan Tran <licki_tung@hotmail.de>
2021-03-08 05:21:06 +0100

    added help controller

---
+++ ASH iOS/Assets/HelpMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    public Button helpButton;
+
+    [SerializeField]
+    public GameObject helpView;
+
+    void Start()
+    {
+        helpView.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (helpView.activeSelf)
+        {
+            CopyPasteSystem.swipeToCopyPaste = false;
+        }
+    }
+
+    public void ShowHideHelpView()
+    {
+        if (helpView.activeSelf)
+        {
+            helpView.SetActive(false);
+        }
+        else
+        {
+            helpView.SetActive(true);
+        }
+    }
+}
