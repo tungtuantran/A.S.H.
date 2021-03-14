494fd52ad017aaa8e4d1e0c8dc01da869ebc3bcf
Tung Tuan Tran <licki_tung@hotmail.de>
2021-03-14 03:45:11 +0100

    update turn all off on image

--- ASH iOS/Assets/Scripts/System/TurnAllOffOnSystem.cs
+++ ASH iOS/Assets/Scripts/System/TurnAllOffOnSystem.cs
@@ -62,7 +62,7 @@ public class TurnAllOffOnSystem : MonoBehaviour
                 disableUIInteractions.DisableInteractions();
 
                 float distance = distanceCalculator.Distance * 100;
-                if (distance > 0.3f)
+                if (distance > 0.6f)                                    //old max distance: 0.3f
                 {
                     TurnAllOffOn();
                 }
