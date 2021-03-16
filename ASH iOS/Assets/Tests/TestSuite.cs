7c70df125175ad8804304bdcbbe83a1ab982aaa9
Tung Tuan Tran <licki_tung@hotmail.de>
2021-03-16 02:37:50 +0100

    add testSuite and new wall lamp marker

--- ASH iOS/Assets/Tests/TestSuite.cs
+++ ASH iOS/Assets/Tests/TestSuite.cs
@@ -8,6 +8,76 @@ namespace Tests
 {
     public class TestSuite
     {
+        private LampController lampController;
+        Device lamp;
+
+        [SetUp]
+        public void SetUp()
+        {
+            /*
+            GameObject root = new GameObject();
+            root.AddComponent<LampController>();
+            lampController = root.GetComponent<LampController>();
+
+            lamp = new Lamp("standing_lamp1", 1, "Lamp 1");
+            lampController.selectedDevice = lamp;
+            */
+        }
+
+
+
+        [TearDown]
+        public void Teardown()
+        {
+            //Object.Destroy(lampController.gameObject);
+            
+        }
+
+        /*
+        [Test]
+        public void SetSelectedDeviceOn()
+        {
+            lampController.SetSelectedDeviceOnOff();
+
+            Assert.IsTrue(lampController.selectedDevice.isOn);
+        }
+
+        [Test]
+        public void SetSelectedDeviceOff()
+        {
+            lampController.SetSelectedDeviceOnOff();
+            lampController.SetSelectedDeviceOnOff();
+
+            Assert.IsFalse(lampController.selectedDevice.isOn);
+        }
+        */
+
+        [Test]
+        public void RemoveSelectedDevice()
+        {
+
+            //setup device collection
+            /*
+            DeviceCollection.DeviceCollectionInstance.AddRegisteredDevice(lamp);
+
+            bool removed = false;
+
+            lampController.RemoveSelectedDevice();
+            if (DeviceCollection.DeviceCollectionInstance.GetRegisteredDeviceByDeviceId(1) == null)
+            {
+                removed = true;
+            }
+            
+            Debug.Log("anzahl: " + DeviceCollection.DeviceCollectionInstance.registeredDevices.Count);
+            Assert.IsTrue(removed);
+            */
+            //DeviceCollection.DeviceCollectionInstance.RemoveRegisteredDevice(DeviceCollection.DeviceCollectionInstance.GetRegisteredDeviceByDeviceId(1));
+            Debug.Log("anzahl: " + DeviceCollection.DeviceCollectionInstance.registeredDevices.Count);
+        }
+
+
+
+        /*
         // A Test behaves as an ordinary method
         [Test]
         public void TestSuiteSimplePasses()
@@ -24,5 +94,6 @@ namespace Tests
             // Use yield to skip a frame.
             yield return null;
         }
+        */
     }
 }
