7742c09a05093d995f29d0386aee70816c8a1588
Tung Tuan Tran <licki_tung@hotmail.de>
2021-03-17 06:12:17 +0100

    add dropdown

---
+++ ASH iOS/Assets/Scripts/GUI/DropdownButton.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownButton : MonoBehaviour
+{
+    public Image arrowDown;
+    public Image arrowUp;
+
+    [SerializeField]
+    public GameObject body;
+
+    [SerializeField]
+    public GameObject footer;
+
+    void Start()
+    {
+        if (arrowDown != null & arrowUp != null)
+        {
+            arrowDown.gameObject.SetActive(true);
+            arrowUp.gameObject.SetActive(false);
+        }
+
+        Collapse();
+    }
+
+    //toggles beteween expand and collabse
+    public void ExpandCollapse()
+    {
+        if (!body.activeSelf)
+        {
+            body.SetActive(true);
+            footer.SetActive(true);
+        }
+        else
+        {
+            body.SetActive(false);
+            footer.SetActive(false);
+        }
+
+        UpdateArrowImage();
+    }
+
+    //gets called by dropdown mananger
+    public void Collapse()
+    {
+        body.SetActive(false);
+        footer.SetActive(false);
+
+        UpdateArrowImage();
+    }
+
+    private void UpdateArrowImage()
+    {
+        if (arrowDown != null & arrowUp != null)
+        {
+            if (!body.activeSelf)
+            {
+                arrowDown.gameObject.SetActive(true);
+                arrowUp.gameObject.SetActive(false);
+            }
+            else
+            {
+                arrowDown.gameObject.SetActive(false);
+                arrowUp.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ ASH iOS/Assets/Scripts/GUI/DropdownManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<DropdownButton> dropdownList;
+
+    private DropdownButton currentlyExpandedDropdown;
+    private bool collapsing;
+
+    
+    void Update()
+    {
+        //pause updating if CollapseAllExceptCurrentlyExpanded() is still running
+        if (!collapsing)
+        {
+            foreach (DropdownButton dropdown in dropdownList)
+            {
+                if (dropdown.body.activeSelf && !dropdown.Equals(currentlyExpandedDropdown))
+                {
+                    currentlyExpandedDropdown = dropdown;
+                    CollapseAllExceptCurrentlyExpanded();
+                }
+            }
+        }
+    }
+    
+
+    public void CollapseAllExceptCurrentlyExpanded()
+    {
+        collapsing = true;
+
+        foreach (DropdownButton dropdown in dropdownList)
+        {
+            if (!dropdown.Equals(currentlyExpandedDropdown))
+            {
+                dropdown.Collapse();
+            }
+        }
+
+        collapsing = false;
+    }
+}
