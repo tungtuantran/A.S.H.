42eed05c267a9a0dd239f5c3e948d4cb32332464
Tung Tuan Tran <licki_tung@hotmail.de>
2021-03-23 01:53:48 +0100

    instruction controller and view updated

---
+++ ASH iOS/Assets/Scripts/GUI/InstructionController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button instructionButton;
+
+    [SerializeField]
+    private Image buttonIconImage;
+
+    [SerializeField]
+    private Sprite instructionIcon;
+
+    [SerializeField]
+    private Sprite closeIcon;
+
+    [SerializeField]
+    private GameObject instructionView;
+
+    void Start()
+    {
+        instructionView.SetActive(false);
+        buttonIconImage.sprite = instructionIcon;
+    }
+
+    public void ShowHideInstructionView()
+    {
+        if (instructionView.activeSelf)
+        {
+            instructionView.SetActive(false);
+            buttonIconImage.sprite = instructionIcon;
+        }
+        else
+        {
+            instructionView.SetActive(true);
+            buttonIconImage.sprite = closeIcon;
+        }
+    }
+}
